9621872b46df73b1f924d436a1fd836ca499c7fc
Internship-Do Van Quyet <do.van.quyet@vsi-international.com>
2024-04-23 12:26:41 +0700

    fix database

--- FrontEnd/src/service/KhachHang/HoaDonService.js
+++ FrontEnd/src/service/KhachHang/HoaDonService.js
@@ -10,9 +10,7 @@ export const checkoutStore = defineStore('checkOut', {
     actions: {
          async checkout(form){
             const response = await axios.post(api, form)
-            console.log("la sao lan thu 1");
-            console.log("asfdl");
-            console.log(response.data);
+
             this.checkOut =  response.data;
          }
        
